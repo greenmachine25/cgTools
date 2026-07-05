a019660b78bbe3a1cbfb26f3b0f794b14f533511
greenmachine25 <greenmachine25@users.noreply.github.com>
2026-07-05 15:21:19 -0400

    Trigger deployment from branch v0.22

--- app.js
+++ app.js
@@ -1687,7 +1687,7 @@ function initVizProfiles(updateCb) {
 }
 
 // Check for updates every 60s
-const CURRENT_VERSION = 'v0.21';
+const CURRENT_VERSION = 'v0.22';
 function checkForUpdates() {
   fetch('./index.html?t=' + Date.now())
     .then(r => r.text())
