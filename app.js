23f26d51692d23fa7b07ef1e5ddd262742ed4027
greenmachine25 <greenmachine25@users.noreply.github.com>
2026-07-05 15:06:43 -0400

    Force push update v0.20

--- app.js
+++ app.js
@@ -1687,7 +1687,7 @@ function initVizProfiles(updateCb) {
 }
 
 // Check for updates every 60s
-const CURRENT_VERSION = 'v0.19';
+const CURRENT_VERSION = 'v0.20';
 function checkForUpdates() {
   fetch('./index.html?t=' + Date.now())
     .then(r => r.text())
